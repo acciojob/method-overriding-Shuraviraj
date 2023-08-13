d46b47eeb6df2ce21ea9592af03efcf2d4d8ca06
Ravi Raj Singh <raviraj.jharkhand@gmail.com>
2023-08-13 14:23:09 +0530

    Assignment done 4

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -3,8 +3,8 @@ package com.driver;
 public class Main {
     public static void main(String[] args) {
         B beta = new B();
-        beta.meth();
+        System.out.println(beta.meth());
 
-        beta.meth();
+        System.out.println(beta.meth());
     }
 }
