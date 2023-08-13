cfeb74fcf7848e238a48f6c45f0842eddc8c39cb
Ravi Raj Singh <raviraj.jharkhand@gmail.com>
2023-08-13 14:46:04 +0530

    "Assignment done 5"

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -4,7 +4,6 @@ public class Main {
     public static void main(String[] args) {
         B beta = new B();
         System.out.println(beta.meth());
-
         System.out.println(beta.meth());
     }
 }
