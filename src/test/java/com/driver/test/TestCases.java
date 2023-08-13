39fe22885667c56f8f9943e203611404e4af3755
Ravi Raj Singh <raviraj.jharkhand@gmail.com>
2023-08-13 15:06:47 +0530

    "Assignment done 10"

--- src/test/java/com/driver/test/TestCases.java
+++ src/test/java/com/driver/test/TestCases.java
@@ -1,4 +1,13 @@
 package com.driver.test;
 
+import org.junit.jupiter.api.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class TestCases {
+    @Test
+    public void sampleTest() {
+        boolean result = true;
+        assertEquals(true, result);
+    }
 }
